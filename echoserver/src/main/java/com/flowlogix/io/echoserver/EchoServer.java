5b452d7cbded39e0d14d36e061c96e1c3dc75ff3
lprimak <lenny@flowlogix.com>
2020-07-29 15:00:13 -0500

    initial commit

---
+++ echoserver/src/main/java/com/flowlogix/io/echoserver/EchoServer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.io.echoserver;
+
+/**
+ *
+ * @author lprimak
+ */
+public class EchoServer {
+    public EchoServer(String localhost, int i) {
+    }
+
+    void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ echoserver/src/main/java/com/flowlogix/io/echoserver/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.io.echoserver;
+
+/**
+ *
+ * @author lprimak
+ */
+public class Main {
+    public static void main(String[] args) {
+        new EchoServer("localhost", 7777).start();
+    }
+}
