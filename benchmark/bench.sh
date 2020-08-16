a873f78dafb762a8e8d44adbad9b39707fae5ebd
lprimak <lenny@flowlogix.com>
2020-08-16 18:35:42 -0500

    restore

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh -p
 # exec ./echo-benchmark -t64 -c10 -e10000 -p 7778 -f bench-input-file 127.0.0.1
 exec ./echo-benchmark -t100 -c10 -e3000 -p 7777 -f bench-input-file 127.0.0.1
-# exec ./echo-benchmark -t6 -c30 -e30 -p 7777 -f bench-input-file 127.0.0.1
+# exec ./echo-benchmark -t20 -c10 -e300 -p 7777 -f bench-input-file 127.0.0.1
 
